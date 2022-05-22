72b54402143de2611fb63bddef594999807384fd
Ayse Bayraktar <ayse.bayraktar@enuygun.com>
2022-05-23 01:23:43 +0300

    change: image path url

--- components/CharacterCard/index.tsx
+++ components/CharacterCard/index.tsx
@@ -4,12 +4,15 @@ import Link from "next/link";
 import { ImageWrapper, NameStyled, CharacterCardWrapper } from "./styled";
 
 const CharacterCard: FC<ICharacterCard> = ({ name, thumbnail, id }) => {
+  const { path } = thumbnail;
+  const pathWithHttps = path.replace(/http/, "https");
+  
   return (
     <CharacterCardWrapper data-testid="CharacterCard__Component">
       <Link href={`/detail/${id}`}>
         <ImageWrapper>
           <NextImage
-            src={thumbnail?.path + "/standard_fantastic.jpg"}
+            src={pathWithHttps + "/standard_fantastic.jpg"}
             priority={true}
             width={250}
             height={250}
